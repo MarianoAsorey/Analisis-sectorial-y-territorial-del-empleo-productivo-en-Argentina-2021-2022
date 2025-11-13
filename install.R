033c2e8130692cb7554ee162b67cf36218a665c0
MarianoAsorey <juedesca@gmail.com>
2025-11-12 22:40:47 -0300

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,17 @@
+# ============================================================
+#   Instalación de paquetes necesarios para el proyecto
+# ============================================================
 
+packages <- c(
+  "tidyverse",
+  "ggcorrplot",
+  "maps",
+  "factoextra",
+  "ggrepel",
+  "janitor",
+  "treemapify",
+  "scales"
+)
+
+# Instala solo los que no estén ya instalados:
+install.packages(setdiff(packages, rownames(installed.packages())))
